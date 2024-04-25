3dc5d78b4a6c62c2e0daef433c674ff9feab2a20
星日语 <admin@weekdaycare.cn>
2024-04-25 05:30:56 +0000

    [feat] adapt to coding tag

--- api/main.py
+++ api/main.py
@@ -3,7 +3,7 @@
 import requests
 import uvicorn
 import re
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pygments import highlight
 from pygments.formatters import HtmlFormatter
@@ -20,13 +20,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/api/v1/generate", response_class=Response)
-def vOneGenerate(response: Response, code: str = "", url: str = "", lang: str = "", withcss: bool = True):
+@app.get("/api/v1/generate")
+def vOneGenerate(code: str = "", url: str = "", lang: str = "", withcss: bool = ""):
     filename = 'InputCode'
     if url:
-        filename = url.split('/')[-1]
-        code = requests.get(url).text
-        code = code.replace('\\', '\\\\')  # 进行转义
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # 确保请求成功
+            code = response.text
+            code = code.replace('\\', '\\\\')  # 进行转义
+            filename = url.split('/')[-1]
+        except requests.RequestException as e:
+            raise HTTPException(status_code=400, detail=str(e)) # 如果请求失败，返回错误信息
 
     try:
         if lang:
@@ -38,15 +43,15 @@ def vOneGenerate(response: Response, code: str = "", url: str = "", lang: str = 
 
     formatter = HtmlFormatter(linenos=True)
     result = highlight(code, lexer, formatter)
-    pattern = re.compile(r'<td class="linenos">.*?</td>', re.DOTALL) # 删除行号
+    result = result.replace('\n', '<br>')  # 保持换行符的 HTML 表示
+    pattern = re.compile(r'<td class="linenos">.*?</td>', re.DOTALL) # 去掉行号
     result = pattern.sub('', result)
     result = result.replace('<div class="highlight">', '<div class="highlight ' + lexer.name.lower() + '"><figcaption><span>' + filename + '</span></figcaption>')
-    result = result.replace('\n', '<br>')[:-4]
-    output = 'document.write(\''+ result + '\') '
-    if withcss:
-        output += '''\ndocument.write('<link rel="stylesheet" href="https://jsd.hzchu.top/gh/thun888/asstes@master/files/pygments-css/default.css">')'''
-    
-    return output
+
+    # 包装结果为 JSON 格式
+    return {
+      "result": result
+    }
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", reload=True)
+    uvicorn.run(app, host="0.0.0.0", reload=True)
